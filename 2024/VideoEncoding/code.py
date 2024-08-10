488d9b715d40b5f5e15a5871cf9a13303ca72673
jkjkil4 <1173374788@qq.com>
2024-08-10 10:43:24 +0800

    update VideoEncoding

--- 2024/VideoEncoding/code.py
+++ 2024/VideoEncoding/code.py
@@ -171,8 +171,6 @@ class Title(Template):
         self.forward()
 
 
-
-
 class SimpleSolution(Template):
     def construct(self) -> None:
         #########################################################
@@ -328,11 +326,11 @@ class SimpleSolution(Template):
         #########################################################
 
         frame0R = frame0_stat.copy()
-        frame0R.digest_styles(color=RED)
+        frame0R.digest_styles(color=[1, 0, 0])
         frame0G = frame0_stat.copy()
-        frame0G.digest_styles(color=GREEN)
+        frame0G.digest_styles(color=[0, 1, 0])
         frame0B = frame0_stat.copy()
-        frame0B.digest_styles(color=BLUE)
+        frame0B.digest_styles(color=[0, 0, 1])
 
         frame0RGB = Group(frame0R, frame0G, frame0B).show()
         frame0RGB.depth.arrange(10)
@@ -799,3 +797,595 @@ class Question(Template):
             stroke_alpha=0.4
         ).show()
         Text('?', font='Noto Sans S Chinese Medium', font_size=80).show()
+
+
+class Limitation(Template):
+    def construct(self) -> None:
+        #########################################################
+
+        video = Video('celeste-playback.mp4', width=Config.get.frame_width * 0.75)
+
+        arrow = Arrow(RIGHT * 4 + DOWN * 1.4, LEFT * 4 + DOWN * 1.4, color=YELLOW)
+
+        bi = ImageItem('bi.jpg', height=Config.get.frame_height)
+
+        #########################################################
+
+        video.seek(3.9)
+        self.play(FadeIn(video, scale=1.5))
+        self.play(
+            video.anim.points.scale(3, about_edge=DOWN).shift(LEFT * 5)
+        )
+        video.start()
+        self.forward(0.5)
+        self.play(
+            video.anim.points.shift(RIGHT * 5)
+        )
+        video.stop()
+        self.play(
+            video.anim.points.shift(RIGHT * 6),
+            duration=0.7
+        )
+        # self.forward(0.2)
+        # self.play(
+        #     video.anim.points.shift(LEFT * 7),
+        #     duration=0.5
+        # )
+        # self.forward(0.2)
+        # self.play(
+        #     video.anim.points.shift(LEFT * 7)
+        # )
+        # self.forward(0.4)
+        # video.stop()
+        self.forward()
+        self.play(GrowArrow(arrow))
+        self.forward()
+        self.play(FocusOn(arrow.points.get_start() + DOWN * 0.5))
+        self.forward(2)
+        bi.show()
+        self.forward()
+
+
+class ComputeAll(Template):
+    def construct(self) -> None:
+        #########################################################
+
+        fire = ImageItem('fire.jpg', alpha=0.3, width=Config.get.frame_width)
+        video = Video('2024-04-26 22-44-23.mp4', width=Config.get.frame_width * 0.5)
+        video.points.scale(1 / 100)
+
+        random.seed(114514)
+
+        line = Line(6 * LEFT, 6 * RIGHT, color=PURPLE_E)
+        unstable_arrow = Arrow(color=PURPLE_A)
+        unstable_arrow.points.set_as_corners([
+            RIGHT * x + UP * (rush_into(y / 3) * 3 * (random.random() * 2 - 1))
+            for x, y in zip(np.linspace(-6, 6, 65), np.linspace(0, 3, 65))
+        ])
+        unstable_arrow.points.make_smooth(root_only=True)
+        unstable_arrow.place_tip()
+
+        #########################################################
+
+        video.start(speed=200)
+
+        self.play(
+            FadeIn(fire),
+            video.anim.points.scale(100),
+            rate_func=linear,
+            duration=2
+        )
+        self.play(
+            video.anim.color.set([1, 0.5, 0])
+        )
+        self.forward()
+        self.play(
+            video.anim(duration=0.4).points.scale(0.5),
+            Create(line),
+            GrowArrow(unstable_arrow),
+            duration=3
+        )
+        self.forward()
+
+
+class RealSolution(Template):
+    def construct(self) -> None:
+        #########################################################
+
+        bgl = Rect(
+            Config.get.frame_width * 2, 2,
+            stroke_alpha=0,
+            fill_alpha=0.5,
+            fill_color=PURPLE_E,
+            depth=1000
+        )
+
+        diff = ImageItem('diff.jpg', height=Config.get.frame_height - 2)
+
+        frames = Group(*[
+            Group(
+                frame,
+                SurroundingRect(frame, color=WHITE, buff=0, stroke_radius=0.01),
+                depth=i
+            )
+            for i, frame in enumerate([
+                VideoFrame('example.mp4', 0, height=Config.get.frame_height - 2),
+                *(diff * 120)
+            ])
+        ])
+
+        for frame in frames:
+            frame.points.to_center().scale(0.3)
+            frame.points.apply_point_fn(
+                lambda p: p + p[0] * 0.6 * DOWN
+            )
+        frames.points.arrange(buff=-0.85).shift(RIGHT * 5)
+
+        origframes = frames[::30]
+        origframes_exf = origframes[1:]
+
+        #########################################################
+
+        self.show(bgl, frames)
+
+        self.forward()
+        self.play(
+            origframes_exf.anim.points.shift(UP),
+            self.camera.anim.points.shift(RIGHT * 4)
+        )
+        self.forward()
+
+        for i, frame in enumerate(origframes_exf, start=1):
+            frame[0].image.set(VideoFrame('example.mp4', i).image.get())
+            frame.points.scale(1.2)
+
+        self.play(
+            *[
+                frame.anim(rate_func=rush_from)
+                    .points.scale(1 / 1.2)
+                for frame in origframes_exf
+            ],
+            duration=0.3
+        )
+
+        self.forward()
+
+        #########################################################
+
+        y = frames[30].points.box.get_y(DOWN)
+
+        def get_start_and_end(i: int):
+            f1 = frames[i * 30]
+            f2 = frames[i * 30 + 29]
+            b1 = f1.points.box
+            b2 = f2.points.box
+            return (
+                [b1.get_x(RIGHT), y, 0],
+                [b2.get_x(RIGHT), y, 0]
+            )
+
+        arrows = Group(*[
+            DoubleArrow(
+                *get_start_and_end(i),
+                buff=0,
+                color=YELLOW,
+                tip_kwargs=dict(
+                    center_anchor=CenterAnchor.Front
+                )
+            )
+            for i in range(4)
+        ]).show()
+
+        txts = Text(
+            '30 帧',
+            color=YELLOW,
+            stroke_alpha=1,
+            stroke_color=BLACK,
+            stroke_background=True
+        ) * len(arrows)
+        for arrow, txt in zip(arrows, txts):
+            txt.points.next_to(arrow, DOWN, buff=0)
+
+        #########################################################
+
+        self.play(
+            *[
+                GrowArrow(arrow)
+                for arrow in arrows
+            ],
+            *[
+                Write(txt)
+                for txt in txts
+            ]
+        )
+
+        self.forward()
+
+        ########################################################
+
+        arrow = Group(
+            Text('跳转'),
+            Arrow(ORIGIN, DOWN),
+            color=PURPLE
+        )
+        arrow.points.arrange(DOWN, buff=SMALL_BUFF)
+        arrow.points.next_to(frames[72], UP, buff=0)
+
+        arrow_actually = Group(
+            Text('跳转（实际上）'),
+            Arrow(ORIGIN, DOWN),
+            color=PURPLE_A
+        )
+        arrow_actually.points.arrange(DOWN, buff=SMALL_BUFF)
+        arrow_actually.points.next_to(frames[60], UP, buff=0)
+
+        tgt = frames[60][0]
+
+        frame72_stat = frames[72].copy()
+
+        frames_stat = frames.copy()
+
+        ########################################################
+
+        self.play(
+            FadeIn(arrow, DOWN, rate_func=rush_from),
+            duration=0.6
+        )
+        self.play(
+            Group(arrow, frames[72])
+                .anim.points.shift(UP * 0.5),
+            duration=0.4
+        )
+        self.forward()
+
+        for frame in frames[61:72]:
+            frame[0].image.set(tgt.image.get())
+            frame.points.scale(1.1)
+            self.play(
+                frame.anim.points.scale(1 / 1.1),
+                duration=0.1,
+                rate_func=rush_from
+            )
+
+        frames[72][0].image.set(VideoFrame('example.mp4', 2.4).image.get())
+        frames[72].points.scale(1.2)
+        self.play(
+            frames[72].anim.points.scale(1 / 1.2),
+            duration=0.2,
+            rate_func=rush_from
+        )
+        self.forward()
+
+        to_diff_lst = []
+
+        def to_diff(frame: Group[ImageItem | SurroundingRect]):
+            to_diff_lst.append(frame)
+            frame[0].image.set(diff.image.get())
+            return frame
+
+        self.play(
+            Aligned(
+                Transform(arrow, arrow_actually, hide_src=False),
+                arrow(VItem).anim.color.set(PURPLE_E),
+                AnimGroup(
+                    FadeTransform(frames[72], frame72_stat),
+                    *[
+                        FadeTransform(
+                            frame,
+                            to_diff(frame.copy())
+                        )
+                        for frame in reversed(frames[61:72])
+                    ],
+                    lag_ratio=0.2
+                ),
+                duration=1.5
+            )
+        )
+        self.forward()
+
+        ########################################################
+
+        exv = Video('pb_crop.mp4', height=6)
+        exv.seek(8.3)
+        exv.points.move_to(self.camera).scale(3, about_edge=DOWN).shift(UP)
+
+        rect = boolean_ops.Difference(
+            FrameRect(),
+            Rect(1.8, 0.7)
+                .points.shift(DR * 0.55 + DOWN * 0.2)
+                .r,
+            stroke_alpha=0,
+            fill_alpha=0.5,
+            fill_color=BLACK
+        )
+        rect.points.move_to(self.camera)
+
+        ########################################################
+
+        exv.start()
+        self.play(FadeIn(exv))
+        self.forward(0.7)
+        exv.stop()
+        self.forward(0.3)
+        self.play(FadeIn(rect))
+        self.forward()
+        self.play(FadeOut(Group(rect, exv)))
+        self.forward()
+
+        ########################################################
+
+        itxt = Text(
+            'I 帧 <fs 0.7><fa 0.5>(Intra-coded Frame)</fa></fs>',
+            color=YELLOW,
+            format=Text.Format.RichText
+        )
+        itxt.points.next_to(frames[30], UP, aligned_edge=LEFT)
+
+        ptxt = Text(
+            'P 帧 <fs 0.7><fa 0.5>(Predicted Frame)</fa></fs>',
+            color=PURPLE,
+            format=Text.Format.RichText
+        )
+        ptxt.points.next_to(frames[40], DOWN, aligned_edge=LEFT)
+
+        ########################################################
+
+        self.play(
+            FadeOut(Group(arrow, arrow_actually,
+                          arrows, txts))
+        )
+        self.forward()
+        self.play(
+            Write(itxt),
+            AnimGroup(
+                *[
+                    frame[1].anim.color.set(YELLOW)
+                        .r.radius.set(0.03)
+                    for frame in origframes
+                ],
+                lag_ratio=0.5,
+                duration=2
+            )
+        )
+        self.forward()
+
+        for f, fs in zip(frames[61:73], frames_stat[61:73]):
+            f.become(fs)
+            f.show()
+        frame72_stat.hide()
+        for f in to_diff_lst:
+            f.hide()
+
+        self.play(
+            Write(ptxt),
+            AnimGroup(
+                *[
+                    frame[1].anim.color.set(PURPLE)
+                        .r.radius.set(0.02)
+                    for i, frame in enumerate(frames)
+                    if i % 30 != 0
+                ],
+                lag_ratio=0.02,
+                duration=2
+            )
+        )
+        self.forward()
+        self.play(
+            Group(itxt, frames[30])
+                .anim.points.shift(DOWN),
+            frames[:30].anim.points.shift(LEFT * 2),
+            frames[60::30].anim.points.shift(RIGHT * 2 + DOWN),
+            Group(frames[61:90], frames[91:120])
+                .anim.points.shift(RIGHT * 2)
+        )
+        self.forward()
+
+        ########################################################
+
+        rect = SurroundingRect(
+            Group(itxt, ptxt, frames[30:60]),
+            buff=Margins(0.5, 0.5, 0.5, 0.9),
+            stroke_color=BLUE,
+            fill_color=BLACK,
+            fill_alpha=0.5
+        )
+
+        gop = Group(
+            Text('GOP', font_size=36, color=BLUE),
+            Text('(Group of Pictures)', font_size=18, color=BLUE_E)
+        )
+        gop.points.arrange(DOWN, buff=SMALL_BUFF)
+        gop.points.move_to(rect)
+
+        btxt = Text(
+            'B 帧 <fs 0.6><fa 0.5>(Bidirectional Predicted Frame)</fa></fs>',
+            color=MAROON,
+            format=Text.Format.RichText
+        )
+        btxt.points.next_to(ptxt, DOWN, aligned_edge=LEFT)
+
+        ########################################################
+
+        self.play(
+            FadeIn(rect),
+            Write(gop)
+        )
+        self.forward()
+        self.play(
+            Write(btxt)
+        )
+        self.forward(2)
+
+
+class Compress(Template):
+    def construct(self) -> None:
+        ########################################################
+
+        box = Square(0.5, stroke_alpha=0, fill_alpha=1, color=GOLD)
+
+        boxes = box * 12
+        boxes.points.arrange_in_grid(n_rows=4, buff=SMALL_BUFF)
+
+        boxgroup = Group(
+            SurroundingRect(boxes, buff=0, stroke_alpha=0, fill_alpha=0.2, fill_color=GOLD_E, depth=10),
+            boxes
+        )
+
+        boxgroups = boxgroup * 8
+        boxgroups.points.arrange(buff=SMALL_BUFF).shift(RIGHT * 2)
+
+        allboxes = Group(*[
+            box
+            for boxgroup in boxgroups[1:]
+            for box in boxgroup[1]
+        ])
+        random.seed(114514)
+        allboxes.shuffle()
+
+        allsur = Group(*[
+            boxgroup[0]
+            for boxgroup in boxgroups[1:]
+        ])
+
+        clip = len(allboxes) * 4 // 5
+
+        uncreate_boxes = allboxes[:clip]
+        stay_boxes = allboxes[clip:]
+        stay_boxes_sorted = Group(
+            *sorted(stay_boxes,
+                    key=lambda box: box.points.box.x)
+        )
+        stay_boxes_sorted_target = stay_boxes_sorted.copy()
+        stay_boxes_sorted_target.points \
+            .arrange_in_grid(n_rows=4, buff=SMALL_BUFF, fill_rows_first=False) \
+            .next_to(boxgroups[0], buff=SMALL_BUFF)
+        stay_boxes_sorted_target(VItem).color.set(PURPLE)
+
+        ########################################################
+
+        self.forward()
+        self.play(
+            *[
+                AnimGroup(
+                    FadeIn(boxgroup[0], scale=0.7, rate_func=rush_from),
+                    Write(boxgroup[1])
+                )
+                for boxgroup in boxgroups
+            ],
+            lag_ratio=0.4,
+            duration=3
+        )
+        self.forward()
+        self.play(
+            Uncreate(uncreate_boxes, lag_ratio=0.1),
+            stay_boxes(VItem).anim.color.set(PURPLE),
+            allsur(VItem).anim.color.set(PURPLE_E),
+            duration=2
+        )
+        self.forward()
+        self.play(Transform(stay_boxes_sorted, stay_boxes_sorted_target))
+
+        self.forward()
+
+        ########################################################
+
+        g = Group(boxgroups[0][1], stay_boxes_sorted_target)
+        gsur = Group(*[
+            boxgroup[0]
+            for boxgroup in boxgroups
+        ])
+
+        computer = ImageItem('computer.png')
+        computer.points.move_to(g)
+
+        internet = Group(
+            Circle(
+                0.6,
+                stroke_alpha=0,
+                fill_alpha=1,
+                fill_color=BLACK
+            ),
+            ImageItem('internet.png', height=1.2)
+        )
+        dst = get_norm(internet.points.box.center - computer.points.box.center)
+
+        computers = Group(
+            computer,
+            *[
+                computer.copy()
+                    .points.move_to(dst * (RIGHT * math.cos(radian) + UP * math.sin(radian)))
+                    .r
+                for i in range(-2, 3)
+                for radian in [i * 60 * DEGREES]
+            ]
+        )
+
+        lines = Group(
+            *[
+                Line(internet, computer)
+                for computer in computers
+            ],
+            color=PURPLE_E,
+        )
+        lines_copy = lines.copy()
+        lines_copy(VItem).color.set(PURPLE_A)
+        lines_copy.depth.arrange(-1)
+
+        line1 = lines_copy[0].copy()
+        line1.points.reverse()
+
+        ########################################################
+
+        self.play(
+            FadeOut(gsur),
+            FadeTransform(g, computer)
+        )
+        self.forward()
+        self.play(
+            FadeIn(internet),
+            FadeIn(computers[1:], scale=0.8, about_point=ORIGIN),
+            Create(lines)
+        )
+        self.prepare(
+            *[
+                ShowPassingFlash(line1, time_width=0.3, rate_func=linear)
+                for _ in range(4)
+            ],
+            lag_ratio=0.7,
+            duration=0.5
+        )
+        self.forward(0.3)
+        self.play(
+            *[
+                ShowPassingFlash(lines_copy[1:], time_width=0.3, rate_func=linear)
+                for _ in range(6)
+            ],
+            lag_ratio=0.7,
+            duration=1
+        )
+        self.forward()
+
+
+class End(Template):
+    def construct(self) -> None:
+        txt1 = Text('这里提及的思路', color=PURPLE_A)
+        txt2 = Text('视频编码', font_size=500, color=BLUE)
+        txt2.points.shift(UP * 12 + RIGHT * 2)
+        rect = Rect(
+            60, 60,
+            color=BLUE,
+            fill_alpha=0.3,
+            stroke_radius=0.2,
+            depth=1
+        )
+        rect.points.move_to(txt2)
+
+        txt2.show()
+        self.forward()
+        self.play(Write(txt1))
+        self.play(
+            self.camera.anim.points.scale(10).move_to(txt2),
+            FadeIn(rect),
+            duration=3
+        )
+        self.forward()
